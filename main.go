de9364affee13509b0e0c84cb23a65091f3c2b03
Iskander Ignatev <iskander.ignatev@gmail.com>
2018-07-13 13:36:24 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"net/http"
+)
+
+func main() {}
+
+type contextKey struct {
+	name string
+}
+
+var contextKeyAPIKey = &contextKey{"api-key"}
+
+//APIKey returns key from context
+func APIKey(ctx context.Context) (string, bool) {
+	key, ok := ctx.Value(contextKeyAPIKey).(string)
+	return key, ok
+}
+
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+		if !isValidAPIKey(key) {
+			responedErr(w, r, http.StatusUnauthorized, "invalid API key")
+			return
+		}
+		ctx := context.WithValue(r.Context(), contextKeyAPIKey, key)
+		fn(w, r.WithContext(ctx))
+	}
+}
+
