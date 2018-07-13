972dbb75a53445d841c96b2c2275c388b9ac0270
Iskander Ignatev <iskander.ignatev@gmail.com>
2018-07-13 15:19:16 +0300

    add polls, respond, main

--- main.go
+++ main.go
@@ -3,9 +3,31 @@ package main
 import (
 	"context"
 	"net/http"
+	"gopkg.in/mgo.v2"
+	"flag"
+	"log"
 )
 
-func main() {}
+func main() {
+	var (
+		addr = flag.String("addr", ":8088", "endpoint address")
+		mongo = flag.String("mongo", "localhost", "mongodb address")
+	)
+	log.Println("Dialing mongo" , *mongo)
+	db, err := mgo.Dial(*mongo)
+	if err != nil {
+		log.Fatalln("failed to connect to mongo:", err)
+	}
+	defer db.Close()
+	s := &Server{
+		db: db,
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/polls/", withCORS(withAPIKey(s.handlePolls)))
+	log.Println("Starting web server on", *addr)
+	http.ListenAndServe(":8088", mux)
+	log.Println("Stopping...")
+}
 
 type contextKey struct {
 	name string
@@ -23,7 +45,7 @@ func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		key := r.URL.Query().Get("key")
 		if !isValidAPIKey(key) {
-			responedErr(w, r, http.StatusUnauthorized, "invalid API key")
+			respondErr(w, r, http.StatusUnauthorized, "invalid API key")
 			return
 		}
 		ctx := context.WithValue(r.Context(), contextKeyAPIKey, key)
@@ -31,3 +53,19 @@ func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+func isValidAPIKey(key string) bool {
+	return key == "abc123"
+}
+
+func withCORS(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+		fn(w, r)
+	}
+}
+
+//Server is the API server.
+type Server struct {
+	db *mgo.Session
+}
